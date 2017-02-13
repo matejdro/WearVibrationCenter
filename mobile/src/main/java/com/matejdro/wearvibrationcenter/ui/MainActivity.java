5428ced82a955e00460ea22ebbfb9e1212c437ad
Matej Drobnič <matejdro@gmail.com>
2017-02-13 09:02:51 +0100

    Moved missing companion notices to wearutils.

--- mobile/src/main/java/com/matejdro/wearvibrationcenter/ui/MainActivity.java
+++ mobile/src/main/java/com/matejdro/wearvibrationcenter/ui/MainActivity.java
@@ -5,19 +5,19 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.matejdro.wearutils.companionnotice.WearCompanionPhoneActivity;
+import com.matejdro.wearvibrationcenter.R;
 import com.matejdro.wearvibrationcenter.common.CommPaths;
 import com.matejdro.wearvibrationcenter.notification.NotificationService;
-import com.matejdro.wearvibrationcenter.R;
 import com.matejdro.wearvibrationcenter.preferences.PerAppSettings;
 
 public class MainActivity extends WearCompanionPhoneActivity implements NavigationView.OnNavigationItemSelectedListener, AppPickerFragment.AppPickerCallback, TitleUtils.TitledActivity, FragmentManager.OnBackStackChangedListener {
--- wear/src/main/java/com/matejdro/wearvibrationcenter/mutepicker/MuteModeActivity.java
+++ wear/src/main/java/com/matejdro/wearvibrationcenter/mutepicker/MuteModeActivity.java
@@ -1,23 +1,14 @@
 package com.matejdro.wearvibrationcenter.mutepicker;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.ResultReceiver;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.wearable.activity.ConfirmationActivity;
 import android.view.View;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.CapabilityApi;
-import com.google.android.gms.wearable.Wearable;
-import com.google.android.wearable.intent.RemoteIntent;
+import com.matejdro.wearutils.companionnotice.WearCompanionWatchActivity;
 import com.matejdro.wearvibrationcenter.R;
-import com.matejdro.wearvibrationcenter.WearCompanionWatchActivity;
 import com.matejdro.wearvibrationcenter.common.CommPaths;
 
 public class MuteModeActivity extends WearCompanionWatchActivity implements GoogleApiClient.ConnectionCallbacks, ResultCallback<CapabilityApi.GetCapabilityResult> {
