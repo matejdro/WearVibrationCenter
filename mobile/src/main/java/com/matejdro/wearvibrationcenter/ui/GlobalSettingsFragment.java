3b6a6ee83f3f0a445b44a5ee957f4264ba128a15
Matej Drobnič <matejdro@gmail.com>
2017-11-04 14:42:18 +0100

    Log retrieval fix

--- mobile/src/main/java/com/matejdro/wearvibrationcenter/ui/GlobalSettingsFragment.java
+++ mobile/src/main/java/com/matejdro/wearvibrationcenter/ui/GlobalSettingsFragment.java
@@ -1,27 +1,20 @@
 package com.matejdro.wearvibrationcenter.ui;
 
 import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.preference.Preference;
 import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.Wearable;
+import com.matejdro.wearutils.logging.LogRetrievalTask;
 import com.matejdro.wearutils.preferences.legacy.CustomStoragePreferenceFragment;
 import com.matejdro.wearutils.preferencesync.PreferencePusher;
 import com.matejdro.wearvibrationcenter.R;
 import com.matejdro.wearvibrationcenter.common.CommPaths;
-
-import java.io.File;
 
 import de.psdev.licensesdialog.LicensesDialog;
 
@@ -47,12 +40,10 @@ public class GlobalSettingsFragment extends CustomStoragePreferenceFragment impl
             }
         });
 
-        findPreference("licenses").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
-        {
+        findPreference("licenses").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 
             @Override
-            public boolean onPreferenceClick(Preference preference)
-            {
+            public boolean onPreferenceClick(Preference preference) {
                 new LicensesDialog.Builder(getActivity())
                         .setNotices(R.raw.notices)
                         .setIncludeOwnLicense(true)
@@ -62,11 +53,9 @@ public class GlobalSettingsFragment extends CustomStoragePreferenceFragment impl
             }
         });
 
-        findPreference("donateButton").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener()
-        {
+        findPreference("donateButton").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
-            public boolean onPreferenceClick(Preference preference)
-            {
+            public boolean onPreferenceClick(Preference preference) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=THRX5EMUNBZE6"));
                 startActivity(intent);
@@ -103,30 +92,10 @@ public class GlobalSettingsFragment extends CustomStoragePreferenceFragment impl
     }
 
     private void sendLogs() {
-        if (ContextCompat.checkSelfPermission(getActivity(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
-            AlertDialog permissionExplanationDialog = new AlertDialog.Builder(getActivity())
-                    .setTitle(R.string.required_permission)
-                    .setMessage(R.string.logs_storage_permission_explanation)
-                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                        @TargetApi(Build.VERSION_CODES.M)
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-                        }
-                    })
-                    .create();
-
-            permissionExplanationDialog.show();
-            return;
-        }
-
-        File targetFile = new File(Environment.getExternalStorageDirectory(), "VibrationCenterLogs.log_zip");
-        /*new LogRetrievalTask(getActivity(),
+        new LogRetrievalTask(getActivity(),
                 CommPaths.COMMAND_SEND_LOGS,
                 "matejdro+support@gmail.com",
-                targetFile).execute((Void) null);*/
+                "com.matejdro.wearvibrationcenter.logs").execute((Void) null);
     }
 
     @Override
