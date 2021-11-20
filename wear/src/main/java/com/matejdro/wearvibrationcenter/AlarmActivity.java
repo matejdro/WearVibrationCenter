c12972ef18f6f4f7251637d214eb4da7b04caf0c
Matej Drobnic <matejdro@gmail.com>
2021-11-20 17:49:09 +0100

    fix: restart on snooze

--- wear/src/main/java/com/matejdro/wearvibrationcenter/AlarmActivity.java
+++ wear/src/main/java/com/matejdro/wearvibrationcenter/AlarmActivity.java
@@ -265,8 +265,6 @@ public class AlarmActivity extends WearableActivity implements View.OnTouchListe
         alarmActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         alarmActivityIntent.putExtra(AlarmActivity.EXTRA_ALARM_COMMAND_BYTES, ParcelPacker.getData(alarmCommand));
 
-        startActivity(alarmActivityIntent);
-
         PendingIntent alarmPendingIntent = PendingIntent.getActivity(this, 0, alarmActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + alarmCommand.getSnoozeDuration(), alarmPendingIntent);
