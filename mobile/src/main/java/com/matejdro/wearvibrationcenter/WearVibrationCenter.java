16e1ce4b518fe33b1a3cb15dbe0fa051d49b9ec1
Matej Drobnic <matejdro@gmail.com>
2022-05-28 07:41:42 +0200

    fix: remove fabric

--- mobile/src/main/java/com/matejdro/wearvibrationcenter/WearVibrationCenter.java
+++ mobile/src/main/java/com/matejdro/wearvibrationcenter/WearVibrationCenter.java
@@ -3,13 +3,9 @@ package com.matejdro.wearvibrationcenter;
 import android.app.Application;
 import android.content.pm.ApplicationInfo;
 
-import com.crashlytics.android.Crashlytics;
 import com.matejdro.wearutils.logging.FileLogger;
-import com.matejdro.wearvibrationcenter.logging.CrashlyticsExceptionWearHandler;
-import com.matejdro.wearvibrationcenter.logging.TimberCrashlytics;
 import com.matejdro.wearvibrationcenter.notification.VibrationCenterChannels;
 
-import io.fabric.sdk.android.Fabric;
 import pl.tajchert.exceptionwear.ExceptionDataListenerService;
 import timber.log.Timber;
 
@@ -21,12 +17,6 @@ public class WearVibrationCenter extends Application {
         Timber.setAppTag("WearVibrationCenter");
 
         boolean isDebuggable = (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
-        if (!isDebuggable) {
-            Fabric.with(this, new Crashlytics());
-            Timber.plant(new TimberCrashlytics());
-            ExceptionDataListenerService.setHandler(new CrashlyticsExceptionWearHandler());
-        }
-
         Timber.plant(new Timber.AndroidDebugTree(isDebuggable));
 
         FileLogger fileLogger = FileLogger.getInstance(this);
